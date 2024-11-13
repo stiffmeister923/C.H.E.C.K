f9959ef99be4a4bfd6a624e21e6225f7b9435025
Didey23 <sosajeshuel23@gmail.com>
2024-11-14 07:28:23 +0800

    added some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
-import { PlusOutlined } from "@ant-design/icons";
+import {
+  PlusOutlined,
+  DownloadOutlined,
+  GithubOutlined,
+  FacebookOutlined,
+  CustomerServiceOutlined,
+  CommentOutlined,
+  QuestionCircleOutlined,
+  SyncOutlined,
+} from "@ant-design/icons";
+
 import {
   Layout,
   Menu,
@@ -13,6 +23,7 @@ import {
   Upload,
 } from "antd";
 import type { GetProp, UploadFile, UploadProps } from "antd";
+import { RcFile } from "antd/lib/upload";
 
 type FileType = Parameters<GetProp<UploadProps, "beforeUpload">>[0];
 const getBase64 = (file: FileType): Promise<string> =>
@@ -33,9 +44,11 @@ const App: React.FC = () => {
   const [previewOpen, setPreviewOpen] = useState(false);
   const [previewImage, setPreviewImage] = useState("");
   const [isManualModalOpen, setIsManualModalOpen] = useState(false);
+  const [isFormattedModalOpen, setIsFormatedModalOpen] = useState(false);
   const [isUploadAnswerKeyOpen, setIsUploadAnswerKeyOpen] = useState(false);
   const [isUploadTestPaperOpen, setIsUploadTestPaperOpen] = useState(false);
   const [isGradeOpen, setIsGradeOpen] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
   const [fileList, setFileList] = useState<UploadFile[]>([]);
 
   const {
@@ -70,6 +83,17 @@ const App: React.FC = () => {
     setIsManualModalOpen(false);
   };
 
+  const onFormattedBtnClick = () => {
+    setIsFormatedModalOpen(true);
+  };
+
+  const handleFormattedOk = () => {
+    setIsFormatedModalOpen(false);
+  };
+  const handleFormattedCancel = () => {
+    setIsFormatedModalOpen(false);
+  };
+
   const onUploadAnswerKeyBtnClick = () => {
     setIsUploadAnswerKeyOpen(true);
   };
@@ -102,7 +126,14 @@ const App: React.FC = () => {
   const handleGradeCancel = () => {
     setIsGradeOpen(false);
   };
-
+  const handleBeforeUpload = (file: RcFile): boolean => {
+    const maxSizeMB = 5; // Set the maximum size in MB
+    if (file.size / 1024 / 1024 > maxSizeMB) {
+      alert(`File is too large! Maximum size is ${maxSizeMB} MB.`);
+      return false;
+    }
+    return true;
+  };
   return (
     <Layout style={{ height: "100vh", background: "#343434" }}>
       <Header
@@ -155,15 +186,41 @@ const App: React.FC = () => {
         </div>
       </Content>
       <Flex wrap gap="small">
-        <FloatButton type="primary" shape="circle" onClick={onManualBtnClick}>
-          A
-        </FloatButton>
+        <FloatButton.Group shape="circle" style={{}}>
+          <FloatButton
+            type="primary"
+            shape="circle"
+            onClick={onManualBtnClick}
+            icon={<QuestionCircleOutlined />}
+          />
+          <FloatButton
+            shape="circle"
+            onClick={onFormattedBtnClick}
+          ></FloatButton>
+        </FloatButton.Group>
         <Modal
           style={{}}
           title="User Manual"
           open={isManualModalOpen}
           onOk={handleManualOk}
           onCancel={handleManualCancel}
+          closable={true}
+          footer={
+            <>
+              <Button
+                type="primary"
+                shape="circle"
+                icon={<GithubOutlined />}
+                size={"large"}
+              />
+              <Button
+                type="primary"
+                shape="circle"
+                icon={<FacebookOutlined />}
+                size={"large"}
+              />
+            </>
+          }
         >
           <p>{isManualModalOpen ? "T" : "F"}</p>
           <p>Some contents...</p>
@@ -171,32 +228,60 @@ const App: React.FC = () => {
         </Modal>
         <Modal
           style={{}}
-          title="Upload Your Answer Key"
-          open={isUploadAnswerKeyOpen}
-          onOk={handleUploadAnswerKeyOk}
-          onCancel={handleUploadAnswerKeyCancel}
+          title="View the Supported Formatted Papers Here"
+          open={isFormattedModalOpen}
+          closable={true}
+          onOk={handleFormattedOk}
+          onCancel={handleFormattedCancel}
+          footer={""}
         >
-          <Upload
-            //action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
-            listType="picture-card"
-            fileList={fileList}
-            onPreview={handlePreview}
-            onChange={handleChange}
-            multiple={true}
+          <Image.PreviewGroup
+            preview={{
+              onChange: (current, prev) =>
+                console.log(`current index: ${current}, prev index: ${prev}`),
+            }}
           >
-            {fileList.length >= 8 ? null : uploadButton}
-          </Upload>
-          {previewImage && (
             <Image
-              wrapperStyle={{ display: "none" }}
-              preview={{
-                visible: previewOpen,
-                onVisibleChange: (visible) => setPreviewOpen(visible),
-                afterOpenChange: (visible) => !visible && setPreviewImage(""),
-              }}
-              src={previewImage}
+              width={200}
+              height={300}
+              src="https://res.cloudinary.com/djdjamrmj/image/upload/v1731539481/not_folder_short4_t6ykm3.jpg"
             />
-          )}
+            <Image
+              width={200}
+              height={300}
+              src="https://res.cloudinary.com/djdjamrmj/image/upload/v1731539479/not_folder_short3_qoybf4.jpg"
+            />
+            <Image
+              width={200}
+              height={300}
+              src="https://res.cloudinary.com/djdjamrmj/image/upload/v1731539479/not_folder_short2_zmfdhc.jpg"
+            />
+            <Image
+              width={200}
+              height={300}
+              src="https://res.cloudinary.com/djdjamrmj/image/upload/v1731539477/not_folder_short1_dzb3aa.jpg"
+            />
+            <Image
+              width={200}
+              height={300}
+              src="https://res.cloudinary.com/djdjamrmj/image/upload/v1731539476/not_folder_long4_d3xfi2.jpg"
+            />
+            <Image
+              width={200}
+              height={300}
+              src="https://res.cloudinary.com/djdjamrmj/image/upload/v1731539476/not_folder_long3_zdrbnc.jpg"
+            />
+            <Image
+              width={200}
+              height={300}
+              src="https://res.cloudinary.com/djdjamrmj/image/upload/v1731539475/not_folder_long1_cestmr.jpg"
+            />
+            <Image
+              width={200}
+              height={300}
+              src="https://res.cloudinary.com/djdjamrmj/image/upload/v1731539475/not_folder_long2_d7z8ci.jpg"
+            />
+          </Image.PreviewGroup>
         </Modal>
         <Modal
           style={{}}
@@ -219,9 +304,6 @@ const App: React.FC = () => {
           <p>Some contents...</p>
         </Modal>
       </Flex>
-      <Footer style={{ textAlign: "center" }}>
-        Ant Design ©{new Date().getFullYear()} Created by Ant UED
-      </Footer>
     </Layout>
   );
 };
